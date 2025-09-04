6b8d5d7c77f0f714bd6f99f3a6aca131855869cf
davcosted <dmitriysuvorov98@mail.ru>
2025-09-04 17:07:14 +0500

    Add files via upload

---
+++ pract1_z1_v13_suvorov_dmitriy/pract1_z1_v13_suvorov_dmitriy/pract1_z1_v13_suvorov_dmitriy.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+int main()
+{   //v13
+    setlocale(LC_ALL, "russian");
+    //int n, i, j = 0, max = 0;
+    //std::cout << "Введите число\n";
+    //std::cin >> n;
+    //for (i = n; i!= 0; j++) 
+    //{
+    //    int b = i % 10;
+    //    i = i / 10;
+    //        if (max < b) 
+    //        {
+    //            max = b;
+    //        }
+    //}
+    //std::cout << "Максимальная цифра в этом числе: ";
+    //std::cout << max;
+    
+    //v21
+    int n;
+    std::cout << "Введите число\n";
+    std::cin >> n;
+    if (n % 2 == 0) {
+        std::cout << "Число чётное";
+    }
+    else {
+        std::cout << "Число нечётное";
+    }
+}
+
